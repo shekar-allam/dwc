ee97d70cc1d1086d861b25e0790b1e0b976c80e5
chandrashekar.allam <chandrashekar.allam@unzer.com>
2025-05-14 12:27:42 +0200

    awefw

--- composeApp/src/wasmJsMain/kotlin/org/unzer/project/HttpClientProvider.kt
+++ composeApp/src/wasmJsMain/kotlin/org/unzer/project/HttpClientProvider.kt
@@ -2,5 +2,17 @@ package org.unzer.project
 
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
-actual val httpClient = HttpClient(Js)
+actual val httpClient = HttpClient(Js) {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
+}
+
